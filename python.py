665f5df4f8bd793b23630d0e86d5af749b0b161c
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-10-31 23:23:51 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,20 @@
+
+'''DATE=31st oct 2022
+DAY= monday
+TOPIC= types of assign the variables
+AUTHOR= POOJA'''
+
+a=1
+print(a)
+a,b,c=1,4,5
+print(a,b,c)
+print(a);print(b);print(c)
+a=b=2;c,d,e=1,3,6
+print(a,c,b,e,d)
+c=" "
+print(c.isspace()) #true
+c=""
+print(c.isspace()) #false
+a='python is high'
+print("value of %s"%a) #value of python is high
+
